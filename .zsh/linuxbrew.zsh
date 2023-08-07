070221f9583c911283e5d7ad33794993ee01045f
Arthur Andrade <git@arthur404.dev>
2023-08-07 16:54:52 -0300

    feat: add mapping to autojump from linuxbrew

--- .zsh/linuxbrew.zsh
+++ .zsh/linuxbrew.zsh
@@ -2,3 +2,5 @@
 eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 # Enable autojump from brew
 [ -f /home/linuxbrew/.linuxbrew/etc/profile.d/autojump.sh ] && . /home/linuxbrew/.linuxbrew/etc/profile.d/autojump.sh
+
+
